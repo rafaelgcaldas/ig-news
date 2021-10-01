07ef5f2c0a32755a0cad4f8c832cd2907475bbf8
Rafael <rafael.gcaldas01@gmail.com>
2021-09-30 23:30:33 -0300

    Redirect to Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/client';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface subscribeButtonProps {
@@ -8,13 +11,23 @@ interface subscribeButtonProps {
 export function SubscribeButton({ priceId }: subscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github');
       return;
     }
 
-    
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId: sessionId });
+
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (

8b240194cd4f3707b39b1fbdb466a38dab533e1e
Rafael <rafael.gcaldas01@gmail.com>
2021-10-14 11:35:56 -0300

    Validating active signature

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,6 +41,18 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req });
   const { slug } = params;
+
+  console.log(session)
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   const prismic = getPrismicClient(req);
 
   const response = await prismic.getByUID('post', String(slug), {});

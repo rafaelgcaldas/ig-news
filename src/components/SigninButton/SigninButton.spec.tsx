df76190d2429bc47cde80f5047e2e08b9575d661
Rafael <rafael.gcaldas01@gmail.com>
2022-01-12 23:03:56 -0300

    Test SubscribeButton component

--- src/components/SigninButton/SigninButton.spec.tsx
+++ src/components/SigninButton/SigninButton.spec.tsx
@@ -8,12 +8,10 @@ jest.mock('next-auth/client')
 describe('SignInButton component', () => {
   it('renders correctly when user is not authenticated', () => {
     const useSessionMocked = mocked(useSession);
-
+    
     useSessionMocked.mockReturnValueOnce([null, false]);
 
-    render (
-      <SignInButton />
-    )
+    render (<SignInButton />)
   
     expect(screen.getByText('Sign in with Github')).toBeInTheDocument()
   })
@@ -22,13 +20,17 @@ describe('SignInButton component', () => {
     const useSessionMocked = mocked(useSession);
 
     useSessionMocked.mockReturnValueOnce([
-      { user: { name: 'John Doe', email: 'johndoe@example.com'}, expires: 'fake-expires'},
+      { 
+        user: { 
+          name: 'John Doe', 
+          email: 'johndoe@example.com'
+        }, 
+        expires: 'fake-expires'
+      },
       false
     ]);
 
-    render (
-      <SignInButton />
-    )
+    render(<SignInButton />)
   
     expect(screen.getByText('John Doe')).toBeInTheDocument()
   })
